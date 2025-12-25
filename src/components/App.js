44750446305e43d3672d4c028ce71764d8885758
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-25 20:09:14 +0530

    Add AuthContext import to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import { AuthContext } from "./AuthContext";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Click on the checkbox to get authenticated</h1>
+      <p>{isAuthenticated ? "You are now authenticated,you can proceed" : "you are not authenticated"}</p>
+      <label><input type="checkbox" checked={isAuthenticated} onChange={toggle}/>I'm not a robot</label>
     </div>
   )
 }
 
-export default App
+export default App;
