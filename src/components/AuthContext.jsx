b7499960a695548e4b6e6e1464f89dfd02da2a17
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-25 20:08:16 +0530

    Update AuthContext.jsx

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,1 +1,19 @@
+import { createContext, useState } from "react";
 
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const [isAuthenticated, setAuthenticated] = useState(false);
+
+    const toggle = () => {
+        return setAuthenticated(!isAuthenticated);
+    }
+
+    return (
+        <AuthContext.Provider value={{isAuthenticated, toggle}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export { AuthContext, AuthProvider};
